bb655cf62d73e50b9c4e9ff5dc88449064647731
Thanuja Ranjeewa <thanuj.tj@gmail.com>
2026-01-21 22:52:44 -0500

    upgrade detection

--- readarr-requests/frontend/src/utils/api.js
+++ readarr-requests/frontend/src/utils/api.js
@@ -8,16 +8,74 @@ const api = axios.create({
   }
 });
 
-// Add token to every request if it exists
+// Add token and app version to every request
 api.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token');
     if (token) {
       config.headers['x-auth-token'] = token;
     }
+    // Add app version header so V3 backend can detect V2 clients
+    config.headers['x-app-version'] = '2';
     return config;
   },
   error => {
+    return Promise.reject(error);
+  }
+);
+
+// Check for V3 upgrade signal in responses
+api.interceptors.response.use(
+  response => {
+    // Check if V3 backend is telling us to upgrade
+    if (response.headers['x-upgrade-required'] === 'true' || 
+        (response.data && response.data._v3_upgrade_required)) {
+      console.log('[V2] V3 upgrade signal received! Clearing caches...');
+      
+      // Clear all caches
+      if ('caches' in window) {
+        caches.keys().then(names => {
+          names.forEach(name => caches.delete(name));
+        });
+      }
+      
+      // Unregister all service workers
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(registrations => {
+          registrations.forEach(reg => reg.unregister());
+        });
+      }
+      
+      // Reload after a short delay
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+    return response;
+  },
+  error => {
+    // Also check error responses for upgrade signal
+    if (error.response && 
+        (error.response.headers['x-upgrade-required'] === 'true' ||
+         (error.response.data && error.response.data._v3_upgrade_required))) {
+      console.log('[V2] V3 upgrade signal received in error! Clearing caches...');
+      
+      if ('caches' in window) {
+        caches.keys().then(names => {
+          names.forEach(name => caches.delete(name));
+        });
+      }
+      
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then(registrations => {
+          registrations.forEach(reg => reg.unregister());
+        });
+      }
+      
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
     return Promise.reject(error);
   }
 );
